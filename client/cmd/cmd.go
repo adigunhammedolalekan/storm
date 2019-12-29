e08ac324d2c1885561750e3454e17aeb77e80877
Lekan Adigun <adigunhammed.lekan@gmail.com>
2019-12-29 03:13:43 +0100

    updated client to include project initialization setup code

--- client/cmd/cmd.go
+++ client/cmd/cmd.go
@@ -1,18 +1,96 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/adigunhammedolalekan/storm/client"
 	"log"
+	"net/url"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
-	cfg := &client.Config{
-		ServerUrl:      "http://localhost:9870",
-		ServerAuthCode: "NmRiOWI2MDctZGU4NS00ZmQyLThiZTItMGE3MjRmMjkwMjVj",
-		AppName:        "testStormApp",
+	args := os.Args
+	if len(args) > 1 {
+		arg := args[1]
+		switch arg {
+		case "init":
+			initProjectConfig()
+		default:
+			runClient()
+		}
 	}
+}
+
+type consoleReader struct {
+}
+
+func (c *consoleReader) readValue(message string) string {
+	buf := bufio.NewReader(os.Stdin)
+	log.Print(message)
+	for {
+		value, err := buf.ReadString(byte('\n'))
+		if err != nil {
+			log.Println("Try again: ")
+			continue
+		}
+		value = strings.Trim(value, "\n")
+		if value == "-q" {
+			return ""
+		}
+		if value != "" {
+			return value
+		}
+	}
+}
+
+func initProjectConfig() {
+	log.Println("Setup Storm: Enter -q to exit")
+	reader := &consoleReader{}
+	name := reader.readValue("Project Name: ")
+	if name == "" {
+		os.Exit(1)
+	}
+	serverUrl := reader.readValue("Server Address: ")
+	if serverUrl == "" {
+		os.Exit(1)
+	}
+	_, err := url.Parse(strings.Trim(serverUrl, "\n"))
+	if err != nil {
+		log.Println("Invalid server url: ", err)
+		os.Exit(1)
+	}
+	serverCode := reader.readValue("Server Authentication Code: ")
+
+	cfg := &client.Config{
+		ServerUrl:      serverUrl,
+		ServerAuthCode: serverCode,
+		AppName:        name,
+	}
+	if err := client.Generate(cfg); err != nil {
+		log.Println("Failed to init project: ", err)
+	}
+	log.Println("Project setup completed successfully")
+	os.Exit(0)
+}
+
+func runClient() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	path := filepath.Join(wd, "storm_config.json")
+	in, err := os.Open(path)
+	if err != nil {
+		log.Fatal("failed to open configuration: ", err)
+	}
+	cfg, err := client.Parse(in)
+	if err != nil {
+		log.Fatal("failed to read configuration: ", err)
+	}
+
 	c, err := client.NewStormClient(cfg)
 	if err != nil {
 		log.Fatal(err)
@@ -22,10 +100,6 @@ func main() {
 		log.Println(err)
 	}
 	log.Println("Starting Deployment...")
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
 	r := &client.DeploymentResult{}
 	if err := c.DeployApp(fmt.Sprintf("%s/%s", wd, cfg.AppName), r); err != nil {
 		log.Println("Deployment failed: ", err)
--- client/config.go
+++ client/config.go
@@ -1,7 +1,36 @@
 package client
 
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
 type Config struct {
 	ServerUrl      string `json:"server_url"`
 	ServerAuthCode string `json:"server_auth_code"`
 	AppName        string `json:"app_name"`
+}
+
+func Parse(r io.Reader) (*Config, error) {
+	cfg := &Config{}
+	if err := json.NewDecoder(r).Decode(cfg); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+func Generate(cfg *Config) error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	path := filepath.Join(wd, "storm_config.json")
+	data, err := json.MarshalIndent(cfg, "", "\t")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(path, data, os.ModePerm)
 }
